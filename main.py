a097b7824dc6cc986e6e3ddff99fff2d19ae1c8d
haru701 <haruhito.matsu@gmail.com>
2023-09-14 17:35:02 +0900

    エンドポイントの追加

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.post("/word")
+async def root1():
+    return {"message": "Word"}
+@app.post("/choiced")
+async def root2():
+    return {"message": "Choiced"}

c15459c643ca4bb01fcc88347b13cf5c38666a5e
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-14 23:45:05 +0900

    update: /wordのロジック実装

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import gensim
+model = gensim.models.KeyedVectors.load_word2vec_format('models/demo2.txt', binary=False)
 
 app = FastAPI()
 
@@ -19,6 +21,12 @@ app.add_middleware(
 class Word(BaseModel):
     word: str
 
+class Attribute(BaseModel):
+    attribute: str
+    
+def getAttribute(word: str) -> Attribute:
+    return Attribute(attribute=model.most_similar(word)[0][0])
+
 class Choiced(BaseModel):
     word:str
     attribute:str
@@ -29,8 +37,8 @@ async def root():
     return {"message": "Hello World"}
 @app.post("/word")
 async def root1(word: Word):
-    word.word = "res-" + word.word
-    return word
+    attr = getAttribute(word=word.word)
+    return attr
 @app.post("/choiced")
 async def root2(choiced: Choiced):
     return choiced
